f1fe33151a2d6c71bb4553836b9acbd7cf06c916
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-04-18 17:14:00 +0100

    Add svg
    - add svg
    - add background colour
    - add google font
    - add animation to light on svg

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-function playKey(e){
-  var dataKey = e.type === 'click' ? e.target.getAttribute("data-key") : e.keyCode;
-  var audio = document.querySelector(`audio[data-key="${dataKey}"]`);
-  var key = document.querySelector(`.key[data-key="${dataKey}"]`);
-  if (!audio){ return; }
-  key.classList.add('playing');
-  audio.currentTime = 0;
-  audio.play();
-};
+(function () {
+  function playKey(e){
+    var dataKey = e.type === 'click' ? e.target.getAttribute("data-key") : e.keyCode;
+    var audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+    var key = document.querySelector(`.key[data-key="${dataKey}"]`);
+    var keyLetter = document.querySelector(`.letter[data-key="${dataKey}"]`);
+    if (!audio){ return; }
+    key.classList.add('playing');
+    keyLetter.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  };
 
-var keys = document.querySelectorAll('.key');
-keys.forEach(function(key){
-  key.addEventListener('transitionend', removeTransition)
-});
+  var keys = document.querySelectorAll('.key');
+  keys.forEach(function(key){
+    key.addEventListener('transitionend', removeTransition)
+  });
 
-function removeTransition(e){
-  this.classList.remove('playing');
-}
+  function removeTransition(e){
+    this.classList.remove('playing');
+  }
 
-document.addEventListener('keydown', playKey);
-document.addEventListener('click', playKey);
+  document.addEventListener('keydown', playKey);
+  document.addEventListener('click', playKey);
+})();
